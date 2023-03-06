207f5406c3f9817392325e6a033b533594829ef3
Devin Jones <devin.jones21083@gmail.com>
2023-03-06 13:02:42 -0500

    Functions added to index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    const cardMessages = [];
+    for (let i = 0; i < names.length; i++) {
+        cardMessages[i] = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    }
+    return cardMessages;
+}
+
+function countDown(numInput) {
+    let output = numInput;
+
+    while (output >= 0) {
+        console.log(output);
+        output--;
+    }
+}
